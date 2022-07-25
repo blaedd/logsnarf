4ba828060b4fd34733ab8176a23edb9a8226832b
David MacKinnon <blaedd@gmail.com>
2022-07-25 23:02:07 +1000

    Clean up the old setup.py, bind to versions that still work with python 2, etc.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('./src'))
 
 from logsnarf import __version__
@@ -10,20 +11,28 @@ setup(
     version=__version__,
     packages=['logsnarf', 'logsnarf.docs'],
     package_dir={'': 'src'},
-    url='http://github.com/blaedd/logsnarf',
+    url='https://github.com/blaedd/logsnarf',
     license='MIT',
     author='David MacKinnon',
     author_email='blaedd@gmail.com',
     description='Tool to stream log files to BigQuery',
     tests_require=['mock', 'setuptools-trial'],
-    setup_requires=['Sphinx'],
+    setup_requires=['Sphinx', 'sphinx-rtd-theme'],
+    python_requires='<3',
     install_requires=[
+        'arrow',
+        'cryptography<=3.3.2',
+        'googleapis-common-protos',
         'python-dateutil',
         'oauth2client',
+        'pathlib',
+        'protobuf',
         'pytz',
         'pyxdg',
+        'rsa<4.6',
+        'requests',
         'httplib2',
-        'google-api-python-client',
+        'google-api-python-client<2',
         'simplejson',
         'pyopenssl',
         'twisted',
